092869e5ed67a1fac2d1864225e6c616cfc56820
Robert McNeill <rjmcneill@users.noreply.github.com>
2016-03-31 14:45:57 +0100

    Added webpack.config and rearranged directory structure

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: [                        // files to run at startup (points are where self-contained scripts go)
+        'babel-polyfill',
+        './src/main/assets/js/main.js',
+        'webpack-dev-server/client?http://localhost:8080'
+    ],
+    output: {                       // where to serve compiled files from
+        publicPath: '/',
+        path: path.join(__dirname, 'public/js'),
+        filename: 'main.js'
+    },
+    devtool: 'source-map',          // serve the source
+    module: {
+        loaders: [                  // list of loaders (where you put things which transform your code)
+            {
+                test: /\.js$/,
+                include: path.join(__dirname, 'src/assets/js'),
+                loader: 'babel-loader',
+                query: {
+                    presets: ["es2015"]
+                }
+            }
+        ]
+    },
+    debug: true,
+    devServer: {
+        contentBase: "./src"
+    }
+};
