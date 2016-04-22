8be5ca14fd2c6503ae73e90b6cf0eab87bc7375d
jdhodges431 <jdhodges431@gmail.com>
2016-04-22 15:22:57 +0100

    Added twitter4j and streaming support

--- src/main/java/tweetscuttler/TweetStreamer.java
+++ src/main/java/tweetscuttler/TweetStreamer.java
@@ -1,82 +1,34 @@
 package tweetscuttler;
 
-import com.twitter.hbc.ClientBuilder;
-import com.twitter.hbc.core.Constants;
-import com.twitter.hbc.core.endpoint.StatusesSampleEndpoint;
-import com.twitter.hbc.core.processor.StringDelimitedProcessor;
-import com.twitter.hbc.httpclient.BasicClient;
-import com.twitter.hbc.httpclient.auth.Authentication;
-import com.twitter.hbc.httpclient.auth.OAuth1;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.TimeUnit;
+import twitter4j.FilterQuery;
+import twitter4j.StatusListener;
+import twitter4j.TwitterStream;
+import twitter4j.TwitterStreamFactory;
+import twitter4j.conf.ConfigurationBuilder;
 
-import org.springframework.beans.factory.annotation.Value;
+public final class TweetStreamer {
+    private TwitterStream twitterStream;
+    private FilterQuery filter;
 
-public enum TweetStreamer {
-	INSTANCE;
+    public TweetStreamer(String consumerKey, String consumerSecret, String token, String secret) {
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey(consumerKey)
+                .setOAuthConsumerSecret(consumerSecret)
+                .setOAuthAccessToken(token)
+                .setOAuthAccessTokenSecret(secret);
 
-	private static BlockingQueue<String> queue = new LinkedBlockingQueue<>(10000);
-
-	private static BasicClient client;
-
-	public String getNextTweet() {
-
-        String msg = null;
-        if (client.isDone()) {
-            System.out.println("Client connection closed unexpectedly: " + client.getExitEvent().getMessage());
-            return null;
-        }
-
-		try {
-			msg = queue.poll(5, TimeUnit.SECONDS);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        if (msg == null) {
-            System.out.println("Did not receive a message in 5 seconds");
-        } else {
-            System.out.println(msg);
-        }
-
-        return msg;
-	}
-	
-	public void run(String consumerKey, String consumerSecret, String token, String secret) throws InterruptedException {
-        // Create an appropriately sized blocking queue
-        
-        // Define our endpoint: By default, delimited=length is set (we need this for our processor)
-        // and stall warnings are on.
-        StatusesSampleEndpoint endpoint = new StatusesSampleEndpoint();
-        endpoint.stallWarnings(false);
-
-        Authentication auth = new OAuth1(consumerKey, consumerSecret, token, secret);
-        //Authentication auth = new com.twitter.hbc.httpclient.auth.BasicAuth(username, password);
-
-        // Create a new BasicClient. By default gzip is enabled.
-        client = new ClientBuilder()
-                .name("sampleExampleClient")
-                .hosts(Constants.STREAM_HOST)
-                .endpoint(endpoint)
-                .authentication(auth)
-                .processor(new StringDelimitedProcessor(queue))
-                .build();
-
-        // Establish a connection
-        client.connect();
-
-        // Print some stats
-        System.out.printf("The client read %d messages!\n", client.getStatsTracker().getNumMessages());
+        twitterStream = new TwitterStreamFactory(cb.build()).getInstance();
     }
 
+    public TweetStreamer addListener(StatusListener sl) {
+        twitterStream.addListener(sl);
+        return this;
+    }
 
-
-    public void start(String consumerKey, String consumerSecret, String token, String secret) {
-	    try {
-	        run(consumerKey, consumerSecret, token, secret);
-	    } catch (InterruptedException | NullPointerException e) {
-	        e.printStackTrace();
-	    }
+    public void applyFilterAndRun(String[] keywords) {
+        filter = new FilterQuery();
+        filter.track(keywords);
+        twitterStream.filter(filter);
     }
 }
--- src/main/scala/tweetscuttler/ChatEventHandler.scala
+++ src/main/scala/tweetscuttler/ChatEventHandler.scala
@@ -1,19 +1,28 @@
 package tweetscuttler
 
-import com.corundumstudio.socketio.AckRequest
-import com.corundumstudio.socketio.SocketIOClient
-import com.corundumstudio.socketio.SocketIOServer
+import com.corundumstudio.socketio.{AckRequest, SocketIOClient, SocketIOServer}
 import com.corundumstudio.socketio.annotation.{OnConnect, OnEvent}
 import org.springframework.beans.factory.annotation.{Autowired, Value}
 import org.springframework.stereotype.Component
+import twitter4j._
 
 @Component class ChatEventHandler {
 
   private final var server: SocketIOServer = null
+  private var streamer: TweetStreamer = null
 
-  @Autowired def this(server: SocketIOServer) {
-    this()
-    this.server = server
+  private val listener: StatusListener = new StatusListener() {
+    override def onStallWarning(warning: StallWarning): Unit = println("sw")
+
+    override def onDeletionNotice(statusDeletionNotice: StatusDeletionNotice): Unit = println("deletion")
+
+    override def onScrubGeo(userId: Long, upToStatusId: Long): Unit = println("scrub")
+
+    override def onStatus(status: Status): Unit = sendStatus(status)
+
+    override def onTrackLimitationNotice(numberOfLimitedStatuses: Int): Unit = println("tln")
+
+    override def onException(ex: Exception): Unit = println("exception")
   }
 
   @Value("${auth.consumer.key}") val consumerKey: String = null
@@ -21,10 +30,25 @@ import org.springframework.stereotype.Component
   @Value("${auth.token}") val token: String = null
   @Value("${auth.secret}") val secret: String = null
 
+  @Autowired def this(server: SocketIOServer) {
+    this()
+    this.server = server
+  }
+
+
+  def sendStatus(status: Status) = {
+    val user = status.getUser.getName
+    val text = status.getText
+    server.getBroadcastOperations.sendEvent("chatevent", ChatObject(user, text))
+  }
+
   @OnConnect
   def onConnectEvent(client: SocketIOClient): Unit = {
     println("on connect")
-    TweetStreamer.INSTANCE.start(consumerKey, consumerSecret, token, secret)
+    if (streamer == null) {
+      streamer = new TweetStreamer(consumerKey, consumerSecret, token, secret)
+        .addListener(listener)
+    }
   }
 
   @OnEvent(value = "chatevent")
@@ -33,6 +57,6 @@ import org.springframework.stereotype.Component
     println(request)
     println(data)
     server.getBroadcastOperations.sendEvent("chatevent", data)
-    server.getBroadcastOperations.sendEvent("chatevent", ChatObject("Twitter", TweetStreamer.INSTANCE.getNextTweet()))
+    streamer.applyFilterAndRun(Array(data.getMessage))
   }
 }
